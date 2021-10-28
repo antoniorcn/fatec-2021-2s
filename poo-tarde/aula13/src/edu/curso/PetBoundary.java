c48cff17c9d008fb8c9c7a536a8367cc824897a1
Antonio Carvalho <antoniorcn@hotmail.com>
2021-10-28 17:54:07 -0300

    POO Tarde Aula 13 parte 2

--- poo-tarde/aula13/src/edu/curso/PetBoundary.java
+++ poo-tarde/aula13/src/edu/curso/PetBoundary.java
@@ -2,6 +2,8 @@ package edu.curso;
 
 import javafx.application.Application;
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -22,9 +24,12 @@ public class PetBoundary extends Application {
     private TextField txtNascimento = new TextField();
 
     private Button btnAdicionar = new Button("Adicionar");
+    private Button btnSalvar = new Button("Salvar");
     private Button btnPesquisar = new Button("Pesquisar");
 
     private PetControl control = new PetControl();   // composição
+
+    private DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     private TableView<Pet> table = new TableView<>();
 
@@ -49,7 +54,20 @@ public class PetBoundary extends Application {
                 new PropertyValueFactory<Pet, Double>("peso")
         );
 
-        table.getColumns().addAll(col1, col2, col3, col4);
+        TableColumn<Pet, String> col5 = new TableColumn<>("Nascimento");
+        col5.setCellValueFactory( (item) -> {
+                    LocalDate d = item.getValue().getNascimento();
+//                    String s = d.format(fmt);
+                    return new ReadOnlyStringWrapper(d.format(fmt));
+                }
+        );
+
+        table.getSelectionModel().selectedItemProperty().addListener( (obs, old, novo) -> {
+                    control.fromEntity(novo);
+                }
+        );
+
+        table.getColumns().addAll(col1, col2, col3, col4, col5);
         table.setItems(control.getLista());
     }
 
@@ -62,6 +80,17 @@ public class PetBoundary extends Application {
         panPrincipal.setCenter(table);
 
         criarTabela();
+
+        txtId.setEditable(false);
+//        txtId.textProperty().addListener( (obs, old, novo) -> {
+//            // if (novo.equals("0")) {  NullPointerException
+//            if ("".trim().equals(novo) || "0".equals(novo)) {
+//                btnSalvar.setVisible(false);
+//            } else {
+//                btnSalvar.setVisible(true);
+//            }
+//
+//        });
 
         Bindings.bindBidirectional(txtId.textProperty(), control.id, new NumberStringConverter());
         Bindings.bindBidirectional(txtNome.textProperty(), control.nome);
@@ -81,19 +110,23 @@ public class PetBoundary extends Application {
         panCampos.add(txtNascimento, 1, 4);
 
         panCampos.add(btnAdicionar, 0, 5);
-        panCampos.add(btnPesquisar, 1, 5);
+        panCampos.add(btnSalvar, 1, 5);
+        panCampos.add(btnPesquisar, 2, 5);
 
 
         btnAdicionar.setOnAction((e) -> {
             control.adicionar();
 //            Alert a = new Alert(Alert.AlertType.INFORMATION, "Pet adicionado com sucesso");
 //            a.showAndWait();
-
-            new Alert(Alert.AlertType.INFORMATION, "Pet adicionado com sucesso").showAndWait();
         });
 
         btnPesquisar.setOnAction( (e) -> {
             control.pesquisar();
+        });
+
+        btnSalvar.setOnAction( (e) -> {
+            control.alterar();
+            new Alert(Alert.AlertType.INFORMATION, "Pet salvo com sucesso").showAndWait();
         });
 
         stage.setScene(scn);
--- poo-tarde/aula13/src/edu/curso/PetControl.java
+++ poo-tarde/aula13/src/edu/curso/PetControl.java
@@ -16,22 +16,58 @@ public class PetControl {
     DoubleProperty peso = new SimpleDoubleProperty(0);
     ObjectProperty nascimento = new SimpleObjectProperty(LocalDate.now());
 
+    private static long counter = 0;
+
     // private List<Pet> pets = new ArrayList<>();
+    private List<Pet> petsGeral = new ArrayList<>();
     private ObservableList<Pet> pets = FXCollections.observableArrayList();
 
     public void adicionar() {
-        Pet p = toEntity();
-        pets.add(p);
-        fromEntity(new Pet());
+//        Pet p = toEntity();
+        Pet p = new Pet();
+        p.setId(++counter);
+        // pets.add(p);
+        fromEntity(p);
     }
 
     public void pesquisar() {
-        for (Pet p : pets) {
+        pets.clear();
+        for (Pet p : petsGeral) {
             if (p.getNome().contains( nome.get() )) {
-                fromEntity(p);
-                break;
+//                fromEntity(p);
+//                break;
+                pets.add(p);
             }
         }
+
+        if (!pets.isEmpty()) {
+            fromEntity(pets.get(0));
+        }
+    }
+
+    public void alterar() {
+        Pet pet = toEntity();
+        boolean encontrado = false;
+        for (int i = 0; i < petsGeral.size(); i++) {
+            Pet p = petsGeral.get(i);
+            if (id.get() == p.getId()) {
+                petsGeral.set(i, pet);
+                encontrado = true;
+
+                this.updateLista();
+            }
+        }
+
+        if (! encontrado) {
+            petsGeral.add(pet);
+
+            this.updateLista();
+        }
+    }
+
+    private void updateLista() {
+        pets.clear();
+        pets.addAll(petsGeral);
     }
 
     public Pet toEntity() {
