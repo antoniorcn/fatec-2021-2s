f63614d6e8eaade061754ebc8f92488fd120ecb0
Antonio Carvalho <antoniorcn@hotmail.com>
2021-11-04 16:34:02 -0300

    POO Tarde Aula 14 parte 1

---
+++ poo-tarde/aula14/src/database.sql
@@ -0,0 +1,14 @@
+CREATE TABLE pet (
+    id int,
+    nome char(100),
+    raca varchar(30),
+    peso double,
+    nascimento date,
+
+    PRIMARY KEY(id)
+);
+
+-- Insere um PET
+
+INSERT INTO pet (id, nome, raca, peso, nascimento)
+VALUES (1, 'Toto', 'Viralata', 15.6, '2018-09-23');
